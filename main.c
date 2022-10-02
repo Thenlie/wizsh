c0697c240cc5442ccd6ef37f9334d9ecd2edd993
Thenlie <leithen113@gmail.com>
2022-10-01 22:26:34 -0600

    Created utils.c. Moved logo print to utils.

--- main.c
+++ main.c
@@ -4,28 +4,7 @@ int get_input(char *buffer);
 
 int main (void)
 {
-    /*
-        * Print opening screen and ASCII art
-        * [0;33m] = yellow
-        * [0;31m] = red
-    */
-    printf("\033[0;33m");
-    printf("                        Welcome to WIZSH!\n");
-    printf("                   A simple shell written in C\n");
-    printf("                     Created by Thenlie 2022\n");
-    printf("\n");
-    printf("\033[0;31m");
-    printf("                     88                      88           \n");
-    printf("                     \"\"                      88           \n");
-    printf("                                             88           \n");
-    printf("  8b      db      d8 88 888888888  ,adPPYba, 88,dPPYba,   \n");
-    printf("  `8b    d88b    d8' 88      a8P\" |8[    \"\"  88P'    \"8a  \n");
-    printf("   `8b  d8'`8b  d8'  88   ,d8P'     `\"Y8ba,  88       88  \n");
-    printf("    `8bd8'  `8bd8'   88 ,d8\"       aa    ]8I 88       88  \n");
-    printf("      YP      YP     88 888888888  `\"YbbdP\"' 88       88  \n");
-    printf("\n");
-    printf("\033[0m"); 
-    // https://ascii.co.uk/
+    print_logo();
 
     char *input_buffer = malloc(sizeof(char) * 256);
 
@@ -64,6 +43,11 @@ int main (void)
         // single word commands
         if (word_count == 1)
         {
+            // lowercase the entire input
+            for (int i = 0; input_buffer[i]; i++)
+            {
+                input_buffer[i] = tolower(input_buffer[i]);
+            }
             if (strcmp(input_buffer, "exit\n") == 0)
             {
                 printf("\ngoodbye <3\n\n");
--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "utils.c"
 
 #define MAX_INPUT 256
 
