3d2c0888e68d6efc073c2157ac010903e0ce5561
Thenlie <leithen113@gmail.com>
2022-10-16 20:11:30 -0600

    Clean up code

--- src/git/git.h
+++ src/git/git.h
@@ -8,6 +8,7 @@ bool is_git_dir(char* dir_path);
 void print_current_branch(void);
 int clone_git_repo(char **input, int word_count);
 int print_git_status(char **input, int word_count);
+int git_branch_command_handler(char** input, int word_count); 
 int print_git_branches(char **input, int word_count);
 int checkout_git_branch(char **input, int word_count);
 int create_git_branch(char **input, int word_count);
