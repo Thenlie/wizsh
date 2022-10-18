35f3db5cd15f14d835bc26a3218584d6d2a66c18
Thenlie <leithen113@gmail.com>
2022-10-17 18:12:38 -0600

    Rename remote command created

--- src/git/git.remote.h
+++ src/git/git.remote.h
@@ -3,8 +3,8 @@
 
 int git_remote_command_handler(char**input, int word_count);
 int print_git_remotes(char **input, int word_count); 
-int git_remote_add(char **input, int word_count);
-int git_remote_remove(char **input, int word_count); 
+int add_git_remote(char **input, int word_count);
+int remove_git_remote(char **input, int word_count); 
 int rename_git_remote(char **input, int word_count);
 
 #endif
