197290b9366ef87a80f8000c40ee68bd10652087
Thenlie <leithen113@gmail.com>
2022-10-21 21:48:23 -0600

    Refactored remote code to only call the repo open once

--- src/handlers/git/git.remote.h
+++ src/handlers/git/git.remote.h
@@ -2,11 +2,5 @@
 #define GIT_REMOTE_H_
 
 int git_remote_command_handler(char**input, int word_count);
-int print_git_remotes(char **input, int word_count); 
-int add_git_remote(char **input, int word_count);
-int remove_git_remote(char **input, int word_count); 
-int rename_git_remote(char **input, int word_count);
-int push_git_remote(char **input, int word_count);
-int pull_git_remote(char **input, int word_count);
 
 #endif
