a84f418a691dcb1b63ea942444c987176f9054bc
Thenlie <leithen113@gmail.com>
2022-10-24 22:55:01 -0600

    git_credential build error

--- src/handlers/git/git.utils.h
+++ src/handlers/git/git.utils.h
@@ -1,8 +1,8 @@
+#include <git2.h>
 #ifndef GIT_UTILS_H_
 #define GIT_UTILS_H_ 
 
 #include <stdbool.h>
-// #include <git2/credential.h>
 
 bool is_git_dir(char* dir_path);
 int credentials_cb(git_credential **out, const char *url, const char *username_from_url, unsigned int allowed_types, void *payload);
